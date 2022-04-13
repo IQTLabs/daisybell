4f7259846c3b50d99899ce256c44c2066a2cfa01
JJ Ben-Joseph <jbenjoseph@iqt.org>
2022-04-13 18:25:07 -0400

    Improve test coverage.

--- tests/test_scanner.py
+++ tests/test_scanner.py
@@ -5,3 +5,9 @@ from transformers import pipeline
 def test_scan():
     res = scan(pipeline("fill-mask", model="roberta-base"))
     assert list(res)[0][0] == "masking-human-bias"
+    res = scan(
+        pipeline(
+            "zero-shot-classification", model="cross-encoder/nli-distilroberta-base"
+        )
+    )
+    assert list(res)[0][0] == "zero-shot-human-bias"
