f8ded62423b1640a036ca7ccfe3bf5fd77848a39
JJ Ben-Joseph <jbenjoseph@iqt.org>
2022-05-23 15:24:41 -0400

    Add a new flag to save output in CSV and JSON.

--- daisybell/__main__.py
+++ daisybell/__main__.py
@@ -1,8 +1,48 @@
+from curses import meta
+from typing import Tuple
+from os import PathLike
+from pathlib import Path
 import json
 from argparse import ArgumentParser
 from transformers import pipeline
-from daisybell import scan
 from tabulate import tabulate
+import pandas as pd
+
+from daisybell import scan
+
+
+def create_file_output(
+    scan_output: Tuple[str, str, str, pd.DataFrame],
+    output_path: PathLike,
+    model_name: str,
+    model_params: dict = None,
+):
+    """
+    Creates scanner output as human and machine readable files.
+
+    Parameters:
+        scan_output: The output of a scan().
+        output_path: A directory to put the files.
+        model_name: The name of the model.
+        model_params: Any parameters for the scanners and model.
+    """
+    Path(output_path).mkdir(exist_ok=True, parents=True)
+    model_metadata = {}
+    model_metadata["name"] = model_name
+    if model_params:
+        model_metadata["params"] = model_params
+    model_metadata["scanners"] = []
+
+    for name, kind, desc, df in scan_output:
+        scan_metadata = {}
+        scan_metadata["name"] = name
+        scan_metadata["kind"] = kind
+        scan_metadata["description"] = desc
+        model_metadata["scanners"].append(scan_metadata)
+        df.to_csv(Path(output_path) / f"{name}.csv", index=False)
+
+    with open(Path(output_path) / "metadata.json", "w") as fd:
+        json.dump(model_metadata, fd)
 
 
 def main():
@@ -21,6 +61,12 @@ def main():
         help="Configure the behavior of scanners",
         type=json.loads,
     )
+    parser.add_argument(
+        "--output",
+        action="store",
+        help="Output scanner results to a directory",
+    )
+
     args = parser.parse_args()
     if args.huggingface and args.task:
         if args.params:
@@ -29,8 +75,12 @@ def main():
             params = {}
         mask = pipeline(args.task, model=args.huggingface)
         print(f"Starting scan on model: {args.huggingface}...")
-        for name, kind, desc, df in scan(mask, params):
+        scan_output = list(scan(mask, params))
+        for name, kind, desc, df in scan_output:
             title = f"Results of {kind} scannner: {name} ({desc})"
             dashes = "=" * len(title)
             print(f"{title}\n{dashes}")
             print(tabulate(df, headers="keys", tablefmt="psql"))
+        if args.output:
+            print(f"Saving output to {args.output}...")
+            create_file_output(scan_output, args.output, args.huggingface, args.params)
