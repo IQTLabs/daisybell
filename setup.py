fb130d7b879fb870a653ed4e77bb1683a0f753c3
JJ Ben-Joseph <jbenjoseph@iqt.org>
2022-05-23 23:08:02 -0400

    Release v0.3.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt") as fd:
 
 setup(
     name="daisybell",
-    version="0.3.0",
+    version="0.3.1",
     description="Scan AI models for problems",
     long_description=open("README.rst").read(),
     keywords="machine_learning artificial_intelligence",
