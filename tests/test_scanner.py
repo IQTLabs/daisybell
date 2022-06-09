fc25479dd60d1e81e82af5be9a2ebd56b89edeb8
JJ Ben-Joseph <jbenjoseph@iqt.org>
2022-06-08 22:13:07 -0400

    Fix tests and make them faster.

--- tests/test_scanner.py
+++ tests/test_scanner.py
@@ -3,14 +3,25 @@ from transformers import pipeline
 
 
 def test_scanning_masking_human_bias():
-    res = scan(pipeline("fill-mask", model="roberta-base"))
-    assert list(res)[0][0] == "masking-human-language-bias"
+    res = scan(
+        pipeline("fill-mask", model="roberta-base"),
+        params={"max_names_per_language": 10},
+    )
+
+    name, kind, _, df = list(res)[0]
+    assert name == "masking-human-language-bias"
+    assert kind == "bias"
+    assert len(df) > 50
 
 
 def test_scanning_zero_shot_human_bias():
     res = scan(
         pipeline(
             "zero-shot-classification", model="cross-encoder/nli-distilroberta-base"
-        )
+        ),
+        params={"max_names_per_language": 10},
     )
-    assert list(res)[0][0] == "zero-shot-human-bias"
+    name, kind, _, df = list(res)[0]
+    assert name == "zero-shot-human-language-bias"
+    assert kind == "bias"
+    assert len(df) > 50
