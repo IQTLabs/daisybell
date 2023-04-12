ecaec83569e05806a8b07c61c4126fd98489b7c7
JJ Ben-Joseph <jj@tensorspace.ai>
2023-04-11 20:43:33 -0400

    Add O labels to sequences and avoid seqeval error

--- daisybell/scanners/ner_language_bias.py
+++ daisybell/scanners/ner_language_bias.py
@@ -1,13 +1,16 @@
+from collections import defaultdict
+from copy import deepcopy
 from logging import Logger
+
 import pandas as pd
 import pysbd
 from seqeval.metrics import recall_score
 from tqdm.auto import tqdm
 from transformers import Pipeline
 
-from daisybell.scanners import ScannerBase
 from daisybell.helpers.common import handle_common_params_to_masking_and_zeroshot
 from daisybell.helpers.dataset import emit_books, replace_entities
+from daisybell.scanners import ScannerBase
 
 NAME = "ner-human-language-bias"
 KIND = "bias"
@@ -48,10 +51,8 @@ class NerLanguageBias(ScannerBase):
             language_names[language] = names
 
         splitter = pysbd.Segmenter(language="en", clean=True)
-        scores = {}
-        control_score = 0
-        control_entities = []
-        test_entities = []
+        control_entities = defaultdict(list)
+        test_entities = defaultdict(list)
         for book_idx, (book_name, book_content) in enumerate(emit_books(params), 1):
             print(f"Scanning using {book_name}...")
             text = splitter.segment(book_content)
@@ -68,7 +69,8 @@ class NerLanguageBias(ScannerBase):
                 for entity in model(sentence):
                     if entity["entity"] == "B-PER":
                         control.append(entity)
-                control_score += len(control)
+                control_entity = [entity["entity"] for entity in control]
+                print(f"==> Control entity: {control_entity}")
 
                 for language in language_names.keys():
                     transformed = replace_entities(
@@ -90,17 +92,37 @@ class NerLanguageBias(ScannerBase):
                         if entity["entity"] == "B-PER":
                             test_result.append(entity["entity"])
 
-                    control_entities.append([entity["entity"] for entity in control])
-                    test_entities.append(test_result)
+                    new_control_entity = deepcopy(control_entity)
+                    if len(control_entity) == 0 and len(test_result) == 0:
+                        continue
+                    elif len(control_entity) > len(test_result):
+                        test_result += ["O"] * (len(control_entity) - len(test_result))
+                    elif len(control_entity) < len(test_result):
+                        new_control_entity += ["O"] * (
+                            len(test_result) - len(control_entity)
+                        )
+                    control_entities[language].append(new_control_entity)
+                    test_entities[language].append(test_result)
 
-                score = recall_score(control_entities, test_entities)
                 if sent_idx == max_sentences_per_book:
                     break
             if book_idx == max_books:
                 break
 
         return (
-            pd.DataFrame({"Language": scores.keys(), "Recall": scores.values()})
+            pd.DataFrame(
+                {
+                    "Language": language_names.keys(),
+                    "Recall": (
+                        recall_score(
+                            control_entities[language],
+                            test_entities[language],
+                            average="macro",
+                        )
+                        for language in language_names.keys()
+                    ),
+                }
+            )
             .sort_values("Recall")
             .reset_index(drop=True)
         )
